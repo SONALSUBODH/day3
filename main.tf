eb6d11fb18e52095099f90ee316453b8a09efa62
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-19 22:24:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,20 +30,19 @@ data "aws_vpc" "default" {
 }
 
 
-resource "aws_lb" "test" {
+resource "aws_lb" "tessubnet-0513597d489e87ee9t" {
   name               = "loadbalancerdemo"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.tf_sg.id]
-  subnets            = ["subnet-0b5fe6746ff2f3430" , "subnet-04fe3b0de658613bc"]
+  subnets            = ["subnet-0513597d489e87ee9" , "subnet-04fe3b0de658613bc"]
 
   enable_deletion_protection = false
+  
   tags = {
     Environment = "production"
   }
 }
-
-
 resource "aws_lb_target_group" "test" {
   name     = "tf-example-lb-tg"
   port     = 80
@@ -65,12 +64,12 @@ resource "aws_lb_target_group" "test2" {
 resource "aws_launch_template" "foobar" {
   name   = "test"
   image_id      = "ami-0f918f7e67a3323f0"
-  instance_type = "t4g.micro"
+  instance_type = "t3.small"
 }
 
 resource "aws_autoscaling_group" "autoscale" {
   name                  = "test-autoscaling-group"  
-  availability_zones    = ["ap-south-1"]
+  availability_zones    = ["ap-south-1b" , "ap-south-1a"]
   desired_capacity      = 3
   max_size              = 6
   min_size              = 3
