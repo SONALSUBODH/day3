f34c3ade1827915466b6aee4d3cf3ed4c93fa5eb
SONALSUBODH <ms.sonalkulkarni@gmail.com>
2025-07-19 18:55:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,43 @@
+resource "aws_security_group" "tf_sg" {
+  name        = "tf_sg"
+  description = "Allow HTTPS to web server"
+  vpc_id      = "vpc-05bdcc8880aab85ab"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    
+  }
+ ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+
+
+
 resource "aws_lb" "test" {
   name               = "loadbalancerdemo"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.lb_sg.id]
+  security_groups    = [aws_security_group" "tf_sg.id]
   subnets            = ["subnet-000d5817b05c45fc2" ,"subnet-0513597d489e87ee9"]
 
   enable_deletion_protection = false
